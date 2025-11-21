978a5de3bb78a38c52fef6b33682741c17451568
clementhansel8891 <greedybugz@gmail.com>
2025-11-22 03:00:50 +0800

    Major revamp to landing page

--- src/pages/api/gohighlevel/route.ts
+++ src/pages/api/gohighlevel/route.ts
@@ -1,36 +1,36 @@
-import { NextResponse } from "next/server";
+// import { NextResponse } from "next/server";
 
-export async function POST(req: Request) {
-  try {
-    const body = await req.json();
+// export async function POST(req: Request) {
+//   try {
+//     const body = await req.json();
 
-    const ghlRes = await fetch("https://rest.gohighlevel.com/v1/contacts/", {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${process.env.GHL_API_KEY}`,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        firstName: body.name,
-        email: body.email,
-        phone: body.phone,
-        source: "Website Contact Form",
-        tags: ["website-lead"],
-        customField: {
-          message: body.message,
-        },
-      }),
-    });
+//     const ghlRes = await fetch("https://rest.gohighlevel.com/v1/contacts/", {
+//       method: "POST",
+//       headers: {
+//         Authorization: `Bearer ${process.env.GHL_API_KEY}`,
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({
+//         firstName: body.name,
+//         email: body.email,
+//         phone: body.phone,
+//         source: "Website Contact Form",
+//         tags: ["website-lead"],
+//         customField: {
+//           message: body.message,
+//         },
+//       }),
+//     });
 
-    if (!ghlRes.ok) {
-      const errorText = await ghlRes.text();
-      console.error("GHL Error:", errorText);
-      return NextResponse.json({ error: "GHL failed" }, { status: 500 });
-    }
+//     if (!ghlRes.ok) {
+//       const errorText = await ghlRes.text();
+//       console.error("GHL Error:", errorText);
+//       return NextResponse.json({ error: "GHL failed" }, { status: 500 });
+//     }
 
-    return NextResponse.json({ success: true });
-  } catch (err) {
-    console.error("Server Error:", err);
-    return NextResponse.json({ error: "Server error" }, { status: 500 });
-  }
-}
+//     return NextResponse.json({ success: true });
+//   } catch (err) {
+//     console.error("Server Error:", err);
+//     return NextResponse.json({ error: "Server error" }, { status: 500 });
+//   }
+// }
